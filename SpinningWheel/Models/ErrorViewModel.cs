b9d62f456fa624c1e075ad345bc1082eae351699
Adela SN <adela.nadhifa14@gmail.com>
2024-01-25 05:33:54 +0700

    initial add

---
+++ SpinningWheel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SpinningWheel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ SpinningWheel/Models/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SpinningWheel.Models
+{
+	public class Video
+	{
+		public Video()
+		{
+		}
+	}
+}
+
